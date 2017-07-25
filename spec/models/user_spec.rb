d06344ba78ca0d9079189c537f41ba76318a01fa
yuraskaz <kazachkoff.yury@yandex.ru>
2017-07-26 00:53:41 +0300

    correct spec/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  context 'validation check' do
-    it { should validate_presence_of :name}
+describe User do
+  #it "validates the name its not empty" do
+    #user = FactoryGirl.create(:user)
+    #user.valid?
+    #user.errors[:name].should be_empty
+  #end
+  before    do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
   end
 end
