902ff661b0dab4c5237fef415557950261af113e
yuraskaz <kazachkoff.yury@yandex.ru>
2017-08-22 21:22:02 +0300

    add_config_mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # config for mailcatcher
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_setting = { address: 'localhost', port: 1025}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
